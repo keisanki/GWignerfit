9e2d12e663df4b0ea88837e623daa322ec070fa3
Florian Schaefer <schaefer@physik.tu-darmstadt.de>
2007-09-17 07:15:12 +0000

    * gwignerfit.c: 	Update copyright years information.

--- src/gwignerfit.c
+++ src/gwignerfit.c
@@ -1,5 +1,5 @@
 /* GWignerFit
- * Copyright (C) 2004, 2005 Florian Schaefer
+ * Copyright (C) 2004-2007 Florian Schaefer
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
